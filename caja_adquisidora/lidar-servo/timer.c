399478010bfc033080407d3549c6ab7bd2836470
Rafael Ignacio Zurita <rafa@fi.uncoma.edu.ar>
2024-04-15 13:16:33 -0300

    mejora en grados

--- caja_adquisidora/lidar-servo/timer.c
+++ caja_adquisidora/lidar-servo/timer.c
@@ -39,12 +39,11 @@ volatile unsigned char *timer1_timsk1 = (unsigned char *)(0x6F);
 #define TIMER1_ICR1H 0x9C /* ICR1 = 40000*/
 #define TIMER1_ICR1L 0x40
 
-int GRADO_CERO=0x02F8;
-//int GRADO_CERO=0x03E8;
+//int GRADO_CERO=0x02F8;
+int GRADO_CERO=0x02A8;
 int GRADO_90=0x1388;
-//int GRADO_180=0x1388;
-//int GRADO_180=0x0D17;
-int GRADO_180=0x0AD6;
+//int GRADO_180=0x0AD6;
+int GRADO_180=0x0D06;
 
 #define IZQ_H 0x03
 #define IZQ_L 0xE8
